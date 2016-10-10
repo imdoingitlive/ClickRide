a2488e851374c5057aff8b8f6d03878b75a786da
Edwin Rivera <edwin.rivera@outlook.com>
2016-10-10 18:54:59 -0400

    created user model with password encryption

---
+++ app/models/user.js
@@ -0,0 +1,43 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+
+// define the schema for the user model
+var userSchema = mongoose.Schema({
+
+  local : {
+    email : String,
+    password : String
+  },
+  facebook : {
+    id : String,
+    token : String,
+    email : String,
+    name : String
+  },
+  twitter : {
+    id : String,
+    token : String,
+    displayName : String,
+    username : String
+  },
+  google : {
+    id : String,
+    token : String,
+    email : String,
+    name : String
+  }
+});
+
+// methods ==========================================
+// generate a hash
+userSchema.methods.generateHash = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+}
+
+// check to see of the password is valid
+userSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.local.password);
+}
+
+// create the model for users and expose it to our app
+module.exports = mongoose.modal('User', userSchema);
