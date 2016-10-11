bd43b0be430f765b1d53fbaec1761ffa47eb648e
Edwin Rivera <edwin.rivera@outlook.com>
2016-10-11 19:22:26 -0400

    set up authentication file

---
+++ config/auth.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+  'facebookAuth' : {
+    'clientID' : 'client-id-here', // App ID
+    'clientSecret' : 'client-secret', // App secret
+    'callbackURL' : 'http://localhost:3000/auth/facebook/callback'
+  },
+
+  'twitterAuth' : {
+    'consumerKey' : 'consumer-key-here',
+    'consumerSecret' : 'consumer-secret',
+    'callbackURL' : 'http://localhost:3000/auth/twitter/callback'
+  },
+
+  'googleAuth' : {
+    'clientID' : 'clientID-here',
+    'clientSecret' : 'client-secret',
+    'callbackURL' : 'http://localhost:3000/auth/google/callback'
+  }
+  
+};
