f9787609042e535b3692397c8ae127fa442810a2
Edwin Rivera <edwin.rivera@outlook.com>
2016-10-12 17:50:10 -0400

    added functionality to link account if user is already logged in

--- config/passport.js
+++ config/passport.js
@@ -119,46 +119,70 @@ module.exports = function(passport){
     clientID : configAuth.facebookAuth.clientID,
     clientSecret : configAuth.facebookAuth.clientSecret,
     callbackURL : configAuth.facebookAuth.callbackURL,
-    profileFields : ["emails", "displayName"]
+    profileFields : ["emails", "displayName"],
+    passReqToCallback : true // allows to pass in the req from the route (check to see if the user is logged in or not)
   },
 
   // facebook will send back the token and profile
-  function(token, refreshToken, profile, done){
+  function(req, token, refreshToken, profile, done){
 
     // asynchronous
     process.nextTick(function(){
       // find the user in the database based on their facebook id
-      User.findOne({ 'facebook.id' : profile.id }, function(err, user){
-        // if err stop everything and return the err
-        // ie err connecting to the db
-        if (err) {
-          return done(err);
-        }
+      
+      // check if the user is already logged in
+      if (!req.user){
 
-        // if the user is found log them in
-        if (user) {
-          return done(null, user); // user found, return that user
-        } else {
-          // if there isnt a user found with the facebook id then create them
-          var newUser = new User();
+        User.findOne({ 'facebook.id' : profile.id }, function(err, user){
+          // if err stop everything and return the err
+          // ie err connecting to the db
+          if (err) {
+            return done(err);
+          }
 
-          // set all of the facebook information in the user model
-          newUser.facebook.id = profile.id; // set the users facebook id
-          newUser.facebook.token = token; // save the token that facebook provides to the user
-          newUser.facebook.name = profile.displayName; // look at the passport user file
-          newUser.facebook.email = profile.emails[0].value; // facebook might return multiple emails so this grabs the first one
+          // if the user is found log them in
+          if (user) {
+            return done(null, user); // user found, return that user
+          } else {
+            // if there isnt a user found with the facebook id then create them
+            var newUser = new User();
 
-          // save the user to the database
-          newUser.save(function(err){
-            if (err){
-              throw err;
-            }
+            // set all of the facebook information in the user model
+            newUser.facebook.id = profile.id; // set the users facebook id
+            newUser.facebook.token = token; // save the token that facebook provides to the user
+            newUser.facebook.name = profile.displayName; // look at the passport user file
+            newUser.facebook.email = profile.emails[0].value; // facebook might return multiple emails so this grabs the first one
 
-            // if successful return the new user
-            return done(null, newUser);
-          });
-        }
-      });
+            // save the user to the database
+            newUser.save(function(err){
+              if (err){
+                throw err;
+              }
+
+              // if successful return the new user
+              return done(null, newUser);
+            });
+          }
+        });
+      } else {
+        // user already exists and is logged in, we have to link accounts
+        var user = req.user; // pull the user out of the session
+
+        // update the current users facebook credentials
+        user.facebook.id = profile.id;
+        user.facebook.token = token;
+        user.facebook.name = profile.displayName;
+        user.facebook.email = profile.emails[0].value;
+
+        // save the user
+        user.save(function(err){
+          if (err) {
+            throw err;
+          }
+
+          return done(null, user);
+        });
+      }
     });
   }));
 
