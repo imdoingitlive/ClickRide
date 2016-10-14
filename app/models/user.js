1770cbfad4ad5993418f8944456aaab3471d23dd
Edwin Rivera <edwin.rivera@outlook.com>
2016-10-14 13:51:13 -0400

    made model for uber

--- app/models/user.js
+++ app/models/user.js
@@ -25,6 +25,12 @@ var userSchema = mongoose.Schema({
     token : String,
     email : String,
     name : String
+  },
+  uber : {
+    id : String,
+    token : String,
+    email : String,
+    name : String
   }
 });
 
