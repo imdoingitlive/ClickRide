aadbe588689d0182c30e7628edfde1209facf471
Edwin Rivera <edwin.rivera@outlook.com>
2016-10-16 19:13:15 -0400

    changed callback URLs to match heroku deploy

--- config/auth_template.js
+++ config/auth_template.js
@@ -3,25 +3,25 @@ module.exports = {
   'facebookAuth' : {
     'clientID' : 'client-id-here', // App ID
     'clientSecret' : 'client-secret', // App secret
-    'callbackURL' : 'http://localhost:3000/auth/facebook/callback'
+    'callbackURL' : 'https://clickride.herokuapp.com/auth/facebook/callback'
   },
 
   'twitterAuth' : {
     'consumerKey' : 'consumer-key-here',
     'consumerSecret' : 'consumer-secret',
-    'callbackURL' : 'http://localhost:3000/auth/twitter/callback'
+    'callbackURL' : 'https://clickride.herokuapp.com/auth/twitter/callback'
   },
 
   'googleAuth' : {
     'clientID' : 'clientID-here',
     'clientSecret' : 'client-secret',
-    'callbackURL' : 'http://localhost:3000/auth/google/callback'
+    'callbackURL' : 'https://clickride.herokuapp.com/auth/google/callback'
   },
 
   'uberAuth' : {
     'clientID' : '',
     'clientSecret' : '',
-    'callbackURL' : 'http://localhost:3000/auth/uber/callback'
+    'callbackURL' : 'https://clickride.herokuapp.com/auth/uber/callback'
   }
     
 };
